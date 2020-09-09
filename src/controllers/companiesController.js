fbf73b1e70eca2b4a8cfb8ee2b4b0bb6208b72a8
Brayan Quirino <brayan.molk5@gmail.com>
2020-09-09 08:22:10 -0300

    Adcionado a listagem de dados de uma company pelo id do provide

--- src/controllers/companiesController.js
+++ src/controllers/companiesController.js
@@ -1,61 +1,64 @@
 const User = require("../models/user");
 const Company = require("../models/company");
-const { APP_URL, PUBLIC_URL} = require("../config/Constants");
+const { APP_URL, PUBLIC_URL } = require("../config/Constants");
 
+const { comparePasswords, generateToken } = require("../utils/AuthUtils");
 
-const { comparePasswords, generateToken } = require('../utils/AuthUtils')
-
-
-function getUrlFromFilename(filename){
-    return path.join(APP_URL, PUBLIC_URL, filename);
+function getUrlFromFilename(filename) {
+  return path.join(APP_URL, PUBLIC_URL, filename);
 }
 
 module.exports = {
-    async store(req, res) {
-        const { _id } = res.locals.user
-        const {
-            nameFantasy,
-            rSocial,
-            cnpj,
-            assignment,
-            responsible,
-            address,
-            contact,
-            description,
-            isActive,
-          } = req.body;
+  async store(req, res) {
+    const { _id } = res.locals.user;
+    const {
+      nameFantasy,
+      rSocial,
+      cnpj,
+      assignment,
+      responsible,
+      address,
+      contact,
+      description,
+      isActive,
+    } = req.body;
 
-          const data = await Company.create({
-            userId: _id,
-            nameFantasy,
-            rSocial,
-            cnpj,
-            assignment,
-            responsible,
-            address,
-            contact,
-            description,
-            isActive,
-          });
-          return res.json(data);
-    },
+    const data = await Company.create({
+      userId: _id,
+      nameFantasy,
+      rSocial,
+      cnpj,
+      assignment,
+      responsible,
+      address,
+      contact,
+      description,
+      isActive,
+    });
+    return res.json(data);
+  },
+  async uploads(req, res) {
+    const { id } = req.params;
 
-    async uploads(req, res) {
-        const { id } = req.params;
+    const cpfFile = req.files.find((value) => value.fieldname === "cpfFile");
+    const cnpjFile = req.files.find((value) => value.fieldname === "cnpjFile");
+    const rgFile = req.files.find((value) => value.fieldname === "rgFile");
 
-        const cpfFile  = req.files.find((value) => value.fieldname === 'cpfFile')
-        const cnpjFile  = req.files.find((value) => value.fieldname === 'cnpjFile')
-        const rgFile  = req.files.find((value) => value.fieldname === 'rgFile')
+    const cpfUrl = getUrlFromFilename(cpfFile.filename);
+    const cnpjUrl = getUrlFromFilename(cnpjFile.filename);
+    const rgUrl = getUrlFromFilename(rgFile.filename);
 
-        const cpfUrl = getUrlFromFilename(cpfFile.filename)
-        const cnpjUrl = getUrlFromFilename(cnpjFile.filename)
-        const rgUrl = getUrlFromFilename(rgFile.filename)
-
-        await Company.findOneAndUpdate(id, 
-        {
-            cpfUrl,
-            cnpjUrl,
-            rgUrl
-        });
-    }
-}
+    await Company.findOneAndUpdate(id, {
+      cpfUrl,
+      cnpjUrl,
+      rgUrl,
+    });
+  },
+  async show(req, res) {
+    const { _id } = res.locals.user;
+    const data = await Company.findOne({
+        userId: _id
+    });
+    return res.json(data)
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -153,8 +153,7 @@ routes.post("/register", authMiddlewares.registerMiddleware,  authenticationsCon
 routes.post("/companies", authMiddlewares.isAuthenticated,  companiesController.store);
 routes.put('/companies/:id/uploads', authMiddlewares.isAuthenticated, 
   multer(multerConfig).any(), uploadProviderController.store);
-// routes.get("/companies", authMiddlewares.isAuthenticated,  companiesController.store);
-// routes.get("/companies/:id", authMiddlewares.isAuthenticated,  companiesController.store);
+routes.get("/companies", authMiddlewares.isAuthenticated,  companiesController.show);
 // routes.put("/companies/:id", authMiddlewares.isAuthenticated,  companiesController.store);
 // routes.delete("/companies/:id", authMiddlewares.isAuthenticated,  companiesController.store);
 
